9ad3de68a872b291b4c4dcaa3c865c3a3ee20a61
MARIA JARAMILLO <mariajaramillo@MARIAs-MacBook-Pro.local>
2024-04-10 19:08:45 -0500

    Project

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('This is an external js file')
+
+
+// This are the strings 
+let num1 = 6 + 3
+let num2 = 7 * 45
+let num3 = 5 - 9
+
+// These are the math equations 
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
